403b5e5019abb593f28704deefc1ca917838ec16
Sam Okasha <okasha86@hotmail.com>
2018-09-24 19:51:41 +0400

    Clean up code in last commit

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,41 +7,38 @@ const User = mongoose.model('User');
 const Faves = mongoose.model('Faves');
 
 module.exports = app => {
-  app.get('/api/posts/:page', (req, res) => {
-    const { page } = req.params;
+  app.get('/api/posts/:page', async (req, res) => {
+    try {
+      const { page } = req.params;
 
-    Post.find({})
+      const posts = await Post.find({})
       .limit(12)
       .skip(12 * page)
       .populate({
         path: '_owner',
         select: 'profilePhoto displayName'
-      })
-      .exec(async (err, posts) => {
-        try {
-          if(req.user) {
-            const doc = await Faves.findOne({_owner: req.user.id}, '_faves', { lean: true });
-            const {_faves} = doc;
-
-            await posts.forEach(post => {
-              if(_faves.toString().includes(post._id)) {
-                console.log('true condition')
-                return post.isFave = true;
-              }
-            });
+      }).exec();
+  
+      if(req.user) {
+        const favesDoc = await Faves.findOne({_owner: req.user.id}, '_faves', { lean: true }); 
+        const {_faves } = favesDoc;
+        
+        posts.forEach(post => {
+          if(_faves.toString().includes(post._id)) {
+            return post.isFave = true;
           }
+        });
+  
+      return res.send(posts);
+      }
 
-          if (err) {
-            res.send(err);
-          } else {
-            res.send(posts);
-          }  
-        } catch(e){
-          console.log(e);
-        }
+      res.send(posts);
 
-      });
+    } catch(e) {
+      console.log(e)
+    }
   });
+
 
   app.post('/api/posts/fave/:id', requireAuth, async (req, res) => {
     try {
