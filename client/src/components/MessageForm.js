fe9ba6a9616e730acdd878d642f5579dbc4fae30
Sam Okasha <okasha86@hotmail.com>
2018-10-12 14:04:08 +0400

    Update MessageForm component: Add styles, validation

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import MailOutlinedIcon from '@material-ui/icons/MailOutlined';
+import Typography from '@material-ui/core/Typography';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 import axios from 'axios';
 
 const styles = theme => ({
   root: {
-    width: '50%',
+    width: '90%',
+    padding: `${theme.spacing.unit * 2}px`,
     position: 'absolute',
     top: '50%',
     left: '50%',
-    backgroundColor: `${theme.palette.background.paper}`,
+    transform: 'translate(-50%, -50%)',
+    [theme.breakpoints.up('sm')]: {
+      top: '20%',
+      transform: 'translate(-50%, 0)',
+      width: '50%'
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '40%'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '30%'
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: '25%'
+    }
+  },
+  heading: {
+    marginBottom: `${theme.spacing.unit}px`
+  },
+  avatar: {
+    margin: `${theme.spacing.unit}px auto`,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    minHeight: '220px',
     display: 'flex',
     flexDirection: 'column',
-    transform: 'translate(-50%, -50%)'
+    justifyContent: 'space-between'
   },
   leftIcon: {
     marginRight: `${theme.spacing.unit}px`
@@ -55,18 +85,50 @@ export class MessageForm extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <form onSubmit={this.onSubmit} className={classes.root}>
-        <OutlinedInput
-          placeholder="Title"
-          type="text"
-          onChange={this.onTitleChange}
-        />
-        <OutlinedInput multiline rows={3} onChange={this.onBodyChange} />
-        <Button type="submit" variant="contained">
-          <SendOutlinedIcon className={classes.leftIcon} />
-          Send
-        </Button>
-      </form>
+      <Paper className={classes.root}>
+        <div className={classes.heading}>
+          <Avatar className={classes.avatar}>
+            <MailOutlinedIcon />
+          </Avatar>
+          <Typography align="center" variant="headline">
+            New Message
+          </Typography>
+        </div>
+        <form className={classes.form} onSubmit={this.onSubmit}>
+          <OutlinedInput
+            placeholder="Title"
+            type="text"
+            onChange={this.onTitleChange}
+            inputProps={{ maxLength: 30 }}
+            endAdornment={
+              <InputAdornment position="end">
+                {this.state.title.length}
+                /30
+              </InputAdornment>
+            }
+          />
+          <OutlinedInput
+            multiline
+            rows={3}
+            onChange={this.onBodyChange}
+            inputProps={{ maxLength: 120 }}
+            endAdornment={
+              <InputAdornment position="end">
+                {this.state.body.length}
+                /120
+              </InputAdornment>
+            }
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={this.state.title.length < 1 || this.state.body.length < 1}
+          >
+            <SendOutlinedIcon className={classes.leftIcon} />
+            Send
+          </Button>
+        </form>
+      </Paper>
     );
   }
 }
