abab43c028a4534b6a003d754c20d7f01e4a7255
samokasha <okasha86@hotmail.com>
2018-10-13 17:36:08 +0400

    Update Profile component: Fix empty links, add validation for empty social link

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,71 +1,71 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
-import compose from 'recompose/compose';
-import Avatar from '@material-ui/core/Avatar';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
-import CancelTwoToneIcon from '@material-ui/icons/CancelTwoTone';
-import axios from 'axios';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import compose from "recompose/compose";
+import Avatar from "@material-ui/core/Avatar";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
+import CancelTwoToneIcon from "@material-ui/icons/CancelTwoTone";
+import axios from "axios";
 
-import ProfileHeader from './ProfileHeader';
-import ProfileNetwork from './ProfileNetwork';
-import { updateProfile } from '../actions/auth';
+import ProfileHeader from "./ProfileHeader";
+import ProfileNetwork from "./ProfileNetwork";
+import { updateProfile } from "../actions/auth";
 
 const styles = theme => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%'
+    display: "flex",
+    justifyContent: "center",
+    width: "100%"
   },
   paper: {
-    width: '90%',
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'relative',
+    width: "90%",
+    display: "flex",
+    flexDirection: "column",
+    position: "relative",
     marginTop: `${theme.spacing.unit * 3}px`,
     padding: `${theme.spacing.unit * 2}px`,
-    [theme.breakpoints.up('sm')]: {
-      width: '65%'
+    [theme.breakpoints.up("sm")]: {
+      width: "65%"
     },
-    [theme.breakpoints.up('md')]: {
-      width: '55%'
+    [theme.breakpoints.up("md")]: {
+      width: "55%"
     },
-    [theme.breakpoints.up('lg')]: {
-      width: '35%'
+    [theme.breakpoints.up("lg")]: {
+      width: "35%"
     }
   },
   editButtons: {
-    position: 'absolute',
-    top: '1%',
-    right: '2%'
+    position: "absolute",
+    top: "1%",
+    right: "2%"
   },
   hideEditButtons: {
-    display: 'none'
+    display: "none"
   },
   avatarContainer: {
-    display: 'flex',
+    display: "flex",
     marginBottom: `${theme.spacing.unit * 2}px`
   },
   userText: {
     marginLeft: `${theme.spacing.unit}px`
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column'
+    display: "flex",
+    flexDirection: "column"
   },
   fieldGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'baseline',
-    justifyContent: 'flex-end',
-    width: '100%%'
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline",
+    justifyContent: "flex-end",
+    width: "100%%"
   },
   socialText: {
     // Stops shaking when edit toggled
@@ -73,20 +73,20 @@ const styles = theme => ({
     marginBottom: `12px`
   },
   textField: {
-    width: '65%',
+    width: "65%",
     marginLeft: `${theme.spacing.unit}px`,
     marginRight: `${theme.spacing.unit * 3}px`
   },
   disabledCursor: {
-    cursor: 'pointer'
+    cursor: "pointer"
   },
   button: {
     marginTop: `${theme.spacing.unit * 2}px`,
-    width: '100%',
-    margin: '0 auto'
+    width: "100%",
+    margin: "0 auto"
   },
   hiddenSubmitButton: {
-    display: 'none'
+    display: "none"
   }
 });
 
@@ -95,18 +95,18 @@ export class Profile extends React.Component {
     super(props);
 
     this.state = {
-      id: '',
+      id: "",
       editEnabled: false,
       ownProfile: false,
-      displayName: this.props.profile ? this.profile.displayName : '',
-      profilePhoto: this.props.profile ? this.profile.profilePhoto : '',
-      joined: this.props.profile ? this.display.joined : '',
-      name: this.props.profile ? this.props.profile.name : '',
-      website: this.props.profile ? this.props.profile.website : '',
-      facebook: this.props.profile ? this.props.profile.facebook : '',
-      gplus: this.props.profile ? this.props.profile.gplus : '',
-      twitter: this.props.profile ? this.props.profile.twitter : '',
-      about: this.props.profile ? this.props.profile.about : ''
+      displayName: this.props.profile ? this.profile.displayName : "",
+      profilePhoto: this.props.profile ? this.profile.profilePhoto : "",
+      joined: this.props.profile ? this.display.joined : "",
+      name: this.props.profile ? this.props.profile.name : "",
+      website: this.props.profile ? this.props.profile.website : "",
+      facebook: this.props.profile ? this.props.profile.facebook : "",
+      gplus: this.props.profile ? this.props.profile.gplus : "",
+      twitter: this.props.profile ? this.props.profile.twitter : "",
+      about: this.props.profile ? this.props.profile.about : ""
     };
   }
 
@@ -114,9 +114,12 @@ export class Profile extends React.Component {
     try {
       const res = await axios.get(`/api/profile/get/${this.props.user}`);
       const { profilePhoto, joined, displayName, profile, _id } = res.data;
-      this.setState({ id: _id, profilePhoto, displayName, joined, ...profile }, () => {
-        return this.checkIfProfileOwner();
-      });
+      this.setState(
+        { id: _id, profilePhoto, displayName, joined, ...profile },
+        () => {
+          return this.checkIfProfileOwner();
+        }
+      );
     } catch (e) {
       console.log(e);
     }
@@ -155,7 +158,7 @@ export class Profile extends React.Component {
           about
         }
       };
-      await axios.post('/api/profile/update', profile);
+      await axios.post("/api/profile/update", profile);
       console.log(profile);
       this.props.updateProfile(profile);
       this.setState({ editEnabled: false });
@@ -184,7 +187,10 @@ export class Profile extends React.Component {
   };
 
   onLinkClick = e => {
-    var win = window.open(`http://${e.target.value}`, '_blank');
+    if (!e.target.value) {
+      return;
+    }
+    var win = window.open(`http://${e.target.value}`, "_blank");
     win.focus();
   };
 
@@ -194,7 +200,7 @@ export class Profile extends React.Component {
     return (
       <div className={classes.root}>
         <Paper className={classes.paper}>
-          <ProfileHeader 
+          <ProfileHeader
             ownProfile={this.state.ownProfile}
             editEnabled={this.state.editEnabled}
             profilePhoto={this.state.profilePhoto}
@@ -204,16 +210,14 @@ export class Profile extends React.Component {
             enableEdit={this.enableEdit}
           />
           <Divider />
-          <ProfileNetwork
-            userid={this.state.id}
-          />
+          <ProfileNetwork userid={this.state.id} />
           <Divider />
           <form onSubmit={this.onSubmit} className={classes.form}>
             <div className={classes.fieldGroup}>
               <Typography variant="body2">Name:</Typography>
               <TextField
                 margin="normal"
-                inputProps={{ style: { color: '#000' } }}
+                inputProps={{ style: { color: "#000" } }}
                 disabled={this.state.editEnabled ? false : true}
                 className={classes.textField}
                 value={this.state.name}
@@ -226,13 +230,13 @@ export class Profile extends React.Component {
               <TextField
                 className={classes.textField}
                 margin="normal"
-                onClick={this.state.editEnabled ? null : this.onLinkClick}
                 inputProps={
-                  this.state.editEnabled
-                    ? {}
-                    : { style: { cursor: 'pointer', color: '#000' } }
+                  !this.state.editEnabled && this.state.website
+                    ? { style: { cursor: "pointer", color: "#3b5999" } }
+                    : {}
                 }
                 disabled={this.state.editEnabled ? false : true}
+                onClick={this.state.editEnabled ? null : this.onLinkClick}
                 value={this.state.website}
                 onChange={this.onWebsiteChange}
               />
@@ -240,15 +244,15 @@ export class Profile extends React.Component {
 
             <div className={classes.fieldGroup}>
               <Typography className={classes.socialText} variant="body2">
-                Facebook:{' '}
+                Facebook:{" "}
               </Typography>
               <TextField
                 margin="normal"
                 className={classes.textField}
                 inputProps={
-                  this.state.editEnabled
-                    ? {}
-                    : { style: { cursor: 'pointer', color: '#3b5999' } }
+                  !this.state.editEnabled && this.state.facebook
+                    ? { style: { cursor: "pointer", color: "#3b5999" } }
+                    : {}
                 }
                 disabled={this.state.editEnabled ? false : true}
                 onClick={this.state.editEnabled ? null : this.onLinkClick}
@@ -259,15 +263,15 @@ export class Profile extends React.Component {
 
             <div className={classes.fieldGroup}>
               <Typography className={classes.socialText} variant="body2">
-                Google+:{' '}
+                Google+:{" "}
               </Typography>
               <TextField
                 margin="normal"
                 className={classes.textField}
                 inputProps={
-                  this.state.editEnabled
-                    ? {}
-                    : { style: { cursor: 'pointer', color: '#dd4b39' } }
+                  !this.state.editEnabled && this.state.gplus
+                    ? { style: { cursor: "pointer", color: "#3b5999" } }
+                    : {}
                 }
                 disabled={this.state.editEnabled ? false : true}
                 onClick={this.state.editEnabled ? null : this.onLinkClick}
@@ -278,15 +282,15 @@ export class Profile extends React.Component {
 
             <div className={classes.fieldGroup}>
               <Typography className={classes.socialText} variant="body2">
-                Twitter:{' '}
+                Twitter:{" "}
               </Typography>
               <TextField
                 margin="normal"
                 className={classes.textField}
                 inputProps={
-                  this.state.editEnabled
-                    ? {}
-                    : { style: { cursor: 'pointer', color: '#55acee' } }
+                  !this.state.editEnabled && this.state.twitter
+                    ? { style: { cursor: "pointer", color: "#3b5999" } }
+                    : {}
                 }
                 disabled={this.state.editEnabled ? false : true}
                 onClick={this.state.editEnabled ? null : this.onLinkClick}
@@ -301,7 +305,7 @@ export class Profile extends React.Component {
                 margin="normal"
                 multiline
                 rows={3}
-                inputProps={{ style: { color: '#000' } }}
+                inputProps={{ style: { color: "#000" } }}
                 className={classes.textField}
                 disabled={this.state.editEnabled ? false : true}
                 value={this.state.about}
