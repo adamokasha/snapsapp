9ff3704f2b996e0c594bd5b1383608285e76d82c
samokasha <okasha86@hotmail.com>
2018-10-16 14:56:12 +0400

    Update CommentForm component: add styles, validation

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,51 +1,75 @@
-import React from 'react';
-import {withStyles} from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
-import OutlinedInput from '@material-ui/core/OutlinedInput'
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const styles = theme => ({
   form: {
-    display: 'flex',
-    flexDirection: 'column'
+    display: "flex",
+    flexDirection: "column"
   }
-})
+});
 
 export class CommentForm extends React.Component {
   state = {
-    commentBody: ''
-  }
+    commentBody: "",
+    isSending: false
+  };
 
-  onSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      await axios.post(`/api/posts/comments/add/${this.props.postId}`, {commentBody: this.state.commentBody});
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({ isSending: true }, async () => {
+      try {
+        await axios.post(`/api/posts/comments/add/${this.props.postId}`, {
+          commentBody: this.state.commentBody
+        });
+        this.setState({ isSending: false, commentBody: "" }, () => {});
+      } catch (e) {
+        console.log(e);
+        this.setState({ isSending: false }, () => {});
+      }
+    });
+  };
 
-  onBodyChange = (e) => {
-    this.setState({commentBody: e.target.value})
-  }
+  onBodyChange = e => {
+    this.setState({ commentBody: e.target.value });
+  };
 
-  render () {
-    const {classes} = this.props;
+  render() {
+    const { classes } = this.props;
 
     return (
       <form onSubmit={this.onSubmit} className={classes.form}>
-        <OutlinedInput onChange={this.onBodyChange} multiline rows={3}></OutlinedInput>
-        <Button variant="contained" color="primary" type="submit">Add Comment</Button>
+        <OutlinedInput
+          onChange={this.onBodyChange}
+          multiline
+          rows={3}
+          value={this.state.commentBody}
+          inputProps={{ maxLength: 120 }}
+          endAdornment={
+            <InputAdornment>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={this.state.commentBody < 1 || this.state.isSending}
+              >
+                Add Comment
+              </Button>
+            </InputAdornment>
+          }
+        />
       </form>
-    )
-  
+    );
   }
 }
 
 CommentForm.propTypes = {
   postId: PropTypes.string.isRequired
-}
+};
 
-export default withStyles(styles)(CommentForm)
+export default withStyles(styles)(CommentForm);
